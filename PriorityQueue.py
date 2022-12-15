6a5cea20350ffed4f88c56c20e000dc94597b9fb
[DVenus19] <[donasco.venus.12232002@gmail.com]>
2022-12-15 22:55:35 +0800

    Commit 20

--- PriorityQueue.py
+++ PriorityQueue.py
@@ -26,6 +26,8 @@ class PriorityQueue:
     def __init__(self):
         self._elements = []
     def enqueue_with_priority(self, priority, value):
+        heappush(self._elements, (-priority,value))
+
 
 
 
