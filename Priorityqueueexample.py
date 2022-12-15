0f131ab354cc2cb2a561619296af84e0c5b12031
[DVenus19] <[donasco.venus.12232002@gmail.com]>
2022-12-15 23:25:43 +0800

    Commit 6

--- Priorityqueueexample.py
+++ Priorityqueueexample.py
@@ -7,3 +7,4 @@ NEUTRAL = 1
 messages = PriorityQueue()
 messages.enqueue_with_priority(IMPORTANT, "Windshield wipers turned on")
 messages.enqueue_with_priority(NEUTRAL, "Radio station tuned in")
+messages.enqueue_with_priority(CRITICAL, "Brake pedal depressed")
