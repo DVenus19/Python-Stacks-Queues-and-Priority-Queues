94d29c9414a79b5ccf8758e180659e51fe8dc50b
[DVenus19] <[donasco.venus.12232002@gmail.com]>
2022-12-15 21:36:57 +0800

    Commit 3

--- Fifo1.py
+++ Fifo1.py
@@ -1,3 +1,9 @@
 from queues2 import Queue
 fifo = Queue("1st","2nd","3rd")
 print("The numbers of queues is ", len(fifo))
+
+
+for element in fifo:
+    print(element)
+
+print("the number of current queues is ", len(fifo))
