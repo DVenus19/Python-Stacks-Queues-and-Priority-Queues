ffef91d30d36f36605b087027409b2be14d3b077
[DVenus19] <[donasco.venus.12232002@gmail.com]>
2022-12-16 20:32:36 +0800

    Commit 4

--- graphtraversal.py
+++ graphtraversal.py
@@ -4,3 +4,6 @@ from graph2 import City, load_graph
 nodes, graph = load_graph("roadmap.dot", City.from_dict)
 
 city1 = nodes["aberdeen"]
+city2 = nodes["perth"]
+
+for i, path in enumerate(nx.all_shortest_paths(graph, city1, city2), 1):
