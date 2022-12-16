61df43cf41059c2dc27195829f869f88428848fb
[DVenus19] <[donasco.venus.12232002@gmail.com]>
2022-12-16 21:41:35 +0800

    Commit 1

--- graph5.py
+++ graph5.py
@@ -3,3 +3,9 @@ import networkx as nx
 from queueMixin import Queue, Stack
 from collections import deque
 
+class City(NamedTuple):
+    name: str
+    country: str
+    year: int
+    latitude: float
+    longitude: float
