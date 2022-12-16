59926f6bae0e1ea4962cad0a1cca57c08d12c0ad
[DVenus19] <[donasco.venus.12232002@gmail.com]>
2022-12-16 22:40:16 +0800

    Commit 3

--- subscriber.py
+++ subscriber.py
@@ -3,3 +3,6 @@ import redis
 with redis.Redis() as client:
     pubsub = client.pubsub()
     pubsub.subscribe("chatroom")
+    for message in pubsub.listen():
+        if message["type"] == "message":
+
