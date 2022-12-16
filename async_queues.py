9ce199209789c7796155a7fed725f93ccbe8061b
[DVenus19] <[donasco.venus.12232002@gmail.com]>
2022-12-16 23:20:57 +0800

    Commit 5

--- async_queues.py
+++ async_queues.py
@@ -89,3 +89,10 @@ def parse_args():
     return parser.parse_args()
 
 
+def display(links):
+    for url, count in links.most_common():
+        print(f"{count:>3} {url}")
+
+
+if __name__ == "__main__":
+    asyncio.run(main(parse_args()))
