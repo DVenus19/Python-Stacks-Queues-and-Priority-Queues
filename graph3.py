f006246e9e7bac32b29698f97625efb718d05bfa
[DVenus19] <[donasco.venus.12232002@gmail.com]>
2022-12-16 20:49:08 +0800

    Commit 3

--- graph3.py
+++ graph3.py
@@ -25,17 +25,3 @@ def load_graph(filename, node_factory):
         name: node_factory(attributes)
         for name, attributes in graph.nodes(data=True)
     }
-    return nodes, nx.Graph(
-        (nodes[name1], nodes[name2], weights)
-        for name1, name2, weights in graph.edges(data=True)
-    )
-def breadth_first_traverse(graph, source):
-    queue = Queue(source)
-    visited = {source}
-    while queue:
-        yield (node := queue.dequeue())
-        for neighbor in graph.neighbors(node):
-            if neighbor not in visited:
-                visited.add(neighbor)
-                queue.enqueue(neighbor)
-
