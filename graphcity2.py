19abd466a6e98c199d9871a63fde762517997a2c
[DVenus19] <[donasco.venus.12232002@gmail.com]>
2022-12-16 22:18:29 +0800

    Commit 2

--- graphcity2.py
+++ graphcity2.py
@@ -4,3 +4,10 @@ from graph import City, load_graph
 def is_twentieth_century(year):
     return year and 1901 <= year <= 2000
 
+nodes, graph = load_graph("roadmap.dot", City.from_dict)
+def order(neighbors):
+    def by_latitude(city):
+        return city.latitude
+    return iter(sorted(neighbors, key=by_latitude, reverse=True))
+
+
