82a787bcab43c226d1ec56179ce5d5e9f1541eaf
[DVenus19] <[donasco.venus.12232002@gmail.com]>
2022-12-16 20:45:22 +0800

    Commit 1

--- graph3.py
+++ graph3.py
@@ -3,3 +3,12 @@ import networkx as nx
 from queueMixin import Queue
 from collections import deque
 
+class City(NamedTuple):
+    name: str
+    country: str
+    year: int
+    latitude: float
+    longitude: float
+
+    @classmethod
+    def from_dict(cls, attrs):
