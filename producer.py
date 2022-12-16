fcb0fd979ef9d35f3585f5aa6bc20caa064ccfec
[DVenus19] <[donasco.venus.12232002@gmail.com]>
2022-12-16 23:00:49 +0800

    Commit 3

--- producer.py
+++ producer.py
@@ -2,3 +2,4 @@ from kafka3 import KafkaProducer
 producer = KafkaProducer(bootstrap_servers="local:9092")
 while True :
     message = input("Message: ")
+    producer.send(topic="datascience", value=message.encode("utf-8"))
