00db144118dbf3bbf7b85a7fe92cd6ef272ea418
[DVenus19] <[donasco.venus.12232002@gmail.com]>
2022-12-16 19:08:46 +0800

    Commit 1

--- maingraph3.py
+++ maingraph3.py
@@ -1,1 +1,4 @@
 from graph import City, load_graph
+nodes, graph = load_graph("roadmap.dot", City.from_dict)
+for neighbor, weights in graph[nodes["london"]].items():
+    print(weights["distance"], neighbor.name)
