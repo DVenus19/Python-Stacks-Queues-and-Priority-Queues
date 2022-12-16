da2d9ac001789ab61855c362f0747d9d680b8a76
[DVenus19] <[donasco.venus.12232002@gmail.com]>
2022-12-16 20:48:26 +0800

    Commit 2

--- graph3.py
+++ graph3.py
@@ -12,3 +12,30 @@ class City(NamedTuple):
 
     @classmethod
     def from_dict(cls, attrs):
+        return cls(
+            name=attrs["xlabel"],
+            country=attrs["country"],
+            year=int(attrs["year"]) or None,
+            latitude=float(attrs["latitude"]),
+            longitude=float(attrs["longitude"]),
+        )
+def load_graph(filename, node_factory):
+    graph = nx.nx_agraph.read_dot(filename)
+    nodes = {
+        name: node_factory(attributes)
+        for name, attributes in graph.nodes(data=True)
+    }
+    return nodes, nx.Graph(
+        (nodes[name1], nodes[name2], weights)
+        for name1, name2, weights in graph.edges(data=True)
+    )
+def breadth_first_traverse(graph, source):
+    queue = Queue(source)
+    visited = {source}
+    while queue:
+        yield (node := queue.dequeue())
+        for neighbor in graph.neighbors(node):
+            if neighbor not in visited:
+                visited.add(neighbor)
+                queue.enqueue(neighbor)
+
