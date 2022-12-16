7004e6b399bec24dbfeae11df6f2674aaa00776a
[DVenus19] <[donasco.venus.12232002@gmail.com]>
2022-12-16 21:34:06 +0800

    Commit 8

--- graph4.py
+++ graph4.py
@@ -32,3 +32,22 @@ def load_graph(filename, node_factory):
 def breadth_first_traverse(graph, source):
     queue = Queue(source)
     visited = {source}
+    while queue:
+        yield (node := queue.dequeue())
+        for neighbor in graph.neighbors(node):
+            if neighbor not in visited:
+                visited.add(neighbor)
+                queue.enqueue(neighbor)
+
+def breadth_first_search(graph, source, predicate):
+    for node in breadth_first_traverse(graph, source):
+        if predicate(node):
+            return node
+
+def shortest_path(graph, source, destination, order_by=None):
+    queue = Queue(source)
+    visited = {source}
+    previous = {}
+    while queue:
+        node = queue.dequeue()
+        neighbors = list(graph.neighbors(node))
